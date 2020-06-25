0635d03d6bc56faf1afcae9f213c07c3daba11af
Vidya-Uma <meenuvidya20@gmail.com>
2020-06-25 15:07:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("home");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love",{thingvar: thing});
+});
+
+app.get("/post", function(req, res){
+    var posts = [
+       {
+         title:'datastructure', 
+         authour:'Vid'
+      },
+      {
+         title:'unix',
+         authour:'Vidya'
+      }
+    ];
+    res.render("posts", {posts:posts});
+});
+
+app.listen(3000, function(req, res){
+   console.log("server started");
+});
